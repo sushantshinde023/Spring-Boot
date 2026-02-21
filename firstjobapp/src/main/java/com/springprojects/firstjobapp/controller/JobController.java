c229b0756ec0c920eeb9af5df41f4a10f0cca033
sushantshinde023 <sushantshinde023@gmail.com>
2026-02-21 15:55:56 +0530

    Add job in memory

--- firstjobapp/src/main/java/com/springprojects/firstjobapp/controller/JobController.java
+++ firstjobapp/src/main/java/com/springprojects/firstjobapp/controller/JobController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springprojects.firstjobapp.entity.Job;
@@ -11,13 +13,19 @@ import com.springprojects.firstjobapp.entity.Job;
 @RestController
 public class JobController {
 	
-	private List<Job> jobs=List.of(new Job(111L,"Java Developer","Should have Java Hands on experience","8L","12L","Pune"));
+	private List<Job> jobs=new ArrayList<>(List.of(new Job(111L,"Java Developer","Should have Java Hands on experience","8L","12L","Pune")));
 	
 	@GetMapping("/jobs")
 	public List<Job> findAll(){
 		
 		
 		return jobs;
+	}
+	
+	@PostMapping("/jobs")
+	public String addJob(@RequestBody Job newJob) {
+		jobs.add(newJob);
+		return "Job Added Successfully";
 	}
 
 }
