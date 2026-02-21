4911f806dc0a6825db3d09dd8ba336732bdbad32
sushantshinde023 <sushantshinde023@gmail.com>
2026-02-21 16:16:59 +0530

    Handling id generation

--- firstjobapp/src/main/java/com/springprojects/firstjobapp/service/JobServiceImpl.java
+++ firstjobapp/src/main/java/com/springprojects/firstjobapp/service/JobServiceImpl.java
@@ -10,7 +10,7 @@ import com.springprojects.firstjobapp.entity.Job;
 @Service
 public class JobServiceImpl implements JobService{
 	private List<Job> jobs=new ArrayList<>(List.of(new Job(111L,"Java Developer","Should have Java Hands on experience","8L","12L","Pune")));
-
+	private Long nextId=2L;
 	@Override
 	public List<Job> findAll() {
 		// TODO Auto-generated method stub
@@ -20,6 +20,7 @@ public class JobServiceImpl implements JobService{
 	@Override
 	public void createJob(Job job) {
 		// TODO Auto-generated method stub
+		job.setId(nextId++);
 		jobs.add(job);
 		
 	}
