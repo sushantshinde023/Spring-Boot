a5022443179ef1883851191dde1d6fb671edebff
sushantshinde023 <sushantshinde023@gmail.com>
2026-03-09 21:52:25 +0530

    implementing fallback for Circuit breaker

--- jobms/src/main/java/com/spring/microservicesproject/jobms/service/JobServiceImpl.java
+++ jobms/src/main/java/com/spring/microservicesproject/jobms/service/JobServiceImpl.java
@@ -1,15 +1,12 @@
 package com.spring.microservicesproject.jobms.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.spring.microservicesproject.jobms.clents.CompanyClient;
 import com.spring.microservicesproject.jobms.clents.ReviewClient;
@@ -35,7 +32,7 @@ public class JobServiceImpl implements JobService {
 	private ReviewClient reviewClient;
 	
 	@Override
-	@CircuitBreaker(name="companyBreaker")
+	@CircuitBreaker(name="companyBreaker", fallbackMethod = "companyBreakerFallback")
 	public List<JobDTO> findAll() {
 		// TODO Auto-generated method stub
 		List<Job> jobs=jobRepository.findAll();
@@ -45,6 +42,11 @@ public class JobServiceImpl implements JobService {
 		//Company company=restTemplate.getForObject("http://localhost:8081/companies/1", Company.class);
 		//System.out.println("++++++++++++++++++++++++++++++++++++"+company.getName());
 		return jobDto;
+	}
+	public List<String> companyBreakerFallback(Exception e){
+		List<String> list = new ArrayList<>();
+		list.add("Dummy");
+		return list;
 	}
 
 	@Override
