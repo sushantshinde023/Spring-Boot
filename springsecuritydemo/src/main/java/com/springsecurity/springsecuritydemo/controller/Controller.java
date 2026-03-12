c1319a4b738d38201017369dc549a879d55fb485
sushantshinde023 <sushantshinde023@gmail.com>
2026-03-12 17:28:34 +0530

    Role Based Authentication

--- springsecuritydemo/src/main/java/com/springsecurity/springsecuritydemo/controller/Controller.java
+++ springsecuritydemo/src/main/java/com/springsecurity/springsecuritydemo/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.springsecurity.springsecuritydemo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,18 @@ public class Controller {
 	
 	@GetMapping("/hello")
 	public String sayHello() {
-		return "Hello";
+		return "Hello user ..!";
 	}
+	@PreAuthorize("hasRole('USER')")
+	@GetMapping("/user")
+	public String sayHelloUser() {
+		return "Hello user ..!";
+	}
+	
+	@PreAuthorize("hasRole('ADMIN')")
+	@GetMapping("/admin")
+	public String sayHelloAdmin() {
+		return "Hello admin ..!";
+	}
+	
 }
