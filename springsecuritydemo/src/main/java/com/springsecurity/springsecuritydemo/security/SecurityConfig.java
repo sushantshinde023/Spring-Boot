f2f1f4df7a64d964755b7f4a54e72d97e4360bde
sushantshinde023 <sushantshinde023@gmail.com>
2026-03-12 21:47:36 +0530

    Security with H2 Database

--- springsecuritydemo/src/main/java/com/springsecurity/springsecuritydemo/security/SecurityConfig.java
+++ springsecuritydemo/src/main/java/com/springsecurity/springsecuritydemo/security/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.springsecurity.springsecuritydemo.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -12,17 +15,25 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+	@Autowired
+	DataSource dataSource;
+	
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+		http.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/h2-console/**").permitAll()
+				.anyRequest().authenticated());
 		http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		http.httpBasic(withDefaults());
+		http.headers(headers-> headers.frameOptions(frameOptions->frameOptions.sameOrigin()));
+		http.csrf(csrf->csrf.disable());
 		return http.build();
 	}
 	
@@ -30,6 +41,11 @@ public class SecurityConfig {
 	public UserDetailsService userDetailsService() {
 		UserDetails user1= User.withUsername("User1").password("{noop}password1").roles("USER").build();
 		UserDetails admin= User.withUsername("admin").password("{noop}adminPassword").roles("ADMIN").build();
-		return new InMemoryUserDetailsManager(user1,admin);
+		//when using Database we have to use JdbcUserDetailsManager it takes DataSource as parameter
+		JdbcUserDetailsManager userDetailsManager= new JdbcUserDetailsManager(dataSource);
+		userDetailsManager.createUser(admin);
+		userDetailsManager.createUser(user1);
+		return userDetailsManager;
+		//return new InMemoryUserDetailsManager(user1,admin);
 	}
 }
