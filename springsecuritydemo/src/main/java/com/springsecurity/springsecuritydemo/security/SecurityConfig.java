9340fc7ed46dda22a5136d5b8ac3b6c33ef9a657
sushantshinde023 <sushantshinde023@gmail.com>
2026-03-12 16:34:00 +0530

    setting session stateless - No JSESSIONID

--- springsecuritydemo/src/main/java/com/springsecurity/springsecuritydemo/security/SecurityConfig.java
+++ springsecuritydemo/src/main/java/com/springsecurity/springsecuritydemo/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,6 +15,7 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+		http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		http.httpBasic(withDefaults());
 		return http.build();
 	}
