c9532a592741a82e66508604e9898eb99a19f00a
sushantshinde023 <sushantshinde023@gmail.com>
2026-03-12 16:23:24 +0530

    Used default method from SpringBootWebSecurityConfiguration without form based authentication

---
+++ springsecuritydemo/src/main/java/com/springsecurity/springsecuritydemo/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.springsecurity.springsecuritydemo.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+		http.httpBasic(withDefaults());
+		return http.build();
+	}
+}
