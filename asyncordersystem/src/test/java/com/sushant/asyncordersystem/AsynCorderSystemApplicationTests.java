c76d6afb1194678c2f78c6684347016199663605
sushantshinde023 <sushantshinde023@gmail.com>
2026-03-14 11:14:29 +0530

    Complete Initial Project Setup

---
+++ asyncordersystem/src/test/java/com/sushant/asyncordersystem/AsynCorderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushant.asyncordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsynCorderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
