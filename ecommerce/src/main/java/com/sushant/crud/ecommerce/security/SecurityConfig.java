f45c3459df839bb470e2b68f43d8b5791e7361ad
sushantshinde023 <sushantshinde023@gmail.com>
2026-03-19 21:32:32 +0530

    security config to allow /api/products/** all others are authenticated

---
+++ ecommerce/src/main/java/com/sushant/crud/ecommerce/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.sushant.crud.ecommerce.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/products/**").permitAll()
+                .anyRequest().authenticated());
+		http.csrf(csrf->csrf.disable());
+		http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		return http.build();
+	}
+
+}
